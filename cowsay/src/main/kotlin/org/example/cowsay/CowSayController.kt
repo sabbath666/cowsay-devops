b7a5a482a71031f8404bb90c2bd71b0a1ea75a70
sergeyzadorozhny <sergeysabbath@gmail.com>
2024-10-23 04:58:26 +0300

    init commit

--- cowsay/src/main/kotlin/org/example/cowsay/CowSayController.kt
+++ cowsay/src/main/kotlin/org/example/cowsay/CowSayController.kt
@@ -1,4 +1,19 @@
 package org.example.cowsay
 
+import com.github.ricksbrown.cowsay.plugin.CowExecutor
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
 class CowSayController {
+
+    @GetMapping("")
+    fun say():String{
+        val cowExecutor = CowExecutor()
+        cowExecutor.setMessage("Hi")
+        cowExecutor.setHtml(true)
+        val result = cowExecutor.execute()
+        return result
+    }
 }
---
+++ infrastructure/monitoring/signoz/install.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+git clone -b main https://github.com/SigNoz/signoz.git && cd signoz/deploy/
+./install.sh
