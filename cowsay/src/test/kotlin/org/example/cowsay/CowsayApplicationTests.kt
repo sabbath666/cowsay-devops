80ae7629a31840a9a1f14b4ff528979c26c3dbe3
sergeyzadorozhny <sergeysabbath@gmail.com>
2024-10-23 11:40:54 +0300

    refactoring

---
+++ cowsay/src/test/kotlin/org/example/cowsay/CowsayApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.cowsay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CowsayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
